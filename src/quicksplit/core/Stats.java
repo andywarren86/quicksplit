28f3a7d799844ecaecd3beca3515e72cc904b2d9
andywarren86@gmail.com <andywarren86@gmail.com@318fb7fe-dbc0-15a3-dd0b-6ff89d1142cb>
2012-07-19 14:23:52 +0000

    Add win/loss streaks to the summary screen

--- src/quicksplit/core/Stats.java
+++ src/quicksplit/core/Stats.java
@@ -1,6 +1,9 @@
 package quicksplit.core;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Represents statistics for a player over a series of games.
@@ -26,6 +29,11 @@ public class Stats
     int evenCount = 0;
     double evenPercent = 0;
     
+    int winStreak = 0;
+    int winStreakTotal = 0;
+    int downStreak = 0;
+    int downStreakTotal = 0;
+    
     public Stats( Player p )
     {
         this( p, QuickSplit.getGameList() );
@@ -38,6 +46,16 @@ public class Stats
     
     public Stats( Player p, Collection<Game> games )
     {
+        int tempWinStreak = 0;
+        int tempWinStreakTotal = 0;
+        List<Integer> winStreaks = new ArrayList<Integer>();
+        List<Integer> winStreakTotals = new ArrayList<Integer>();
+        
+        int tempDownStreak = 0;
+        int tempDownStreakTotal = 0;
+        List<Integer> downStreaks = new ArrayList<Integer>();
+        List<Integer> downStreakTotals = new ArrayList<Integer>();
+        
         for( Game g : games )
         {
             if( g.getPlayers().contains( p ) )
@@ -51,19 +69,50 @@ public class Stats
                     winCount++;
                     winTotal += r.getAmount();
                     mostWon = r.getAmount() > mostWon ? r.getAmount() : mostWon;
+                    
+                    tempWinStreak++;
+                    tempWinStreakTotal += r.getAmount();
+                    
+                    downStreaks.add( tempDownStreak );
+                    downStreakTotals.add( tempDownStreakTotal );
+                    tempDownStreak = 0;
+                    tempDownStreakTotal = 0;
                 }
                 else if( r.getAmount() < 0 )
                 {
                     lostCount++;
                     lostTotal += r.getAmount();
                     mostLost = r.getAmount() < mostLost ? r.getAmount() : mostLost;
+                    
+                    winStreaks.add( tempWinStreak );
+                    winStreakTotals.add( tempWinStreakTotal );
+                    tempWinStreak = 0;
+                    tempWinStreakTotal = 0;
+                    
+                    tempDownStreak++;
+                    tempDownStreakTotal += r.getAmount();
                 }
                 else
                 {
                     evenCount++;
+                    
+                    winStreaks.add( tempWinStreak );
+                    winStreakTotals.add( tempWinStreakTotal );
+                    tempWinStreak = 0;
+                    tempWinStreakTotal = 0;
+                    
+                    downStreaks.add( tempDownStreak );
+                    downStreakTotals.add( tempDownStreakTotal );
+                    tempDownStreak = 0;
+                    tempDownStreakTotal = 0;
                 }
             }
         }
+        
+        winStreaks.add( tempWinStreak );
+        winStreakTotals.add( tempWinStreakTotal );
+        downStreaks.add( tempDownStreak );
+        downStreakTotals.add( tempDownStreakTotal );
         
         average = Double.valueOf( total ) / Double.valueOf( count );
         winPercent = Double.valueOf( winCount ) / Double.valueOf( count );
@@ -71,6 +120,16 @@ public class Stats
         evenPercent = Double.valueOf( evenCount ) / Double.valueOf( count );
         averageWon = Double.valueOf( winTotal ) / Double.valueOf( winCount );
         averageLost = Double.valueOf( lostTotal ) / Double.valueOf( lostCount );
+        
+        Collections.sort( winStreaks, Collections.reverseOrder() );
+        Collections.sort( winStreakTotals, Collections.reverseOrder() );
+        winStreak = winStreaks.isEmpty() ? 0 : winStreaks.get( 0 );
+        winStreakTotal = winStreakTotals.isEmpty() ? 0 : winStreakTotals.get( 0 );
+        
+        Collections.sort( downStreaks, Collections.reverseOrder() );
+        Collections.sort( downStreakTotals );
+        downStreak = downStreaks.isEmpty() ? 0 : downStreaks.get( 0 );
+        downStreakTotal = downStreakTotals.isEmpty() ? 0 : downStreakTotals.get( 0 );
     }
     
     public int getCount()
@@ -146,5 +205,25 @@ public class Stats
     public double getEvenPercent()
     {
         return evenPercent;
+    }
+
+    public int getWinStreak()
+    {
+        return winStreak;
+    }
+
+    public int getWinStreakTotal()
+    {
+        return winStreakTotal;
+    }
+
+    public int getDownStreak()
+    {
+        return downStreak;
+    }
+
+    public int getDownStreakTotal()
+    {
+        return downStreakTotal;
     }
 }
