ac2bcce86fdad0e1afbd2403f5b362098ce663a8
Andrew <andywarren86@gmail.com>
2016-04-20 20:41:47 +1000

    WIP

---
+++ src/main/resources/export.sql
@@ -0,0 +1,4 @@
+call csvwrite( 'data\player.csv', 'select * from player' );
+call csvwrite( 'data\season.csv', 'select * from season' );
+call csvwrite( 'data\game.csv', 'select * from game' );
+call csvwrite( 'data\result.csv', 'select * from result' );
