1d22ac4ede944683c28759d311c473c008949667
Andrew Slappendel <andywarren86@gmail.com>
2016-06-27 15:36:21 +1000

    Insert transaction data

--- src/main/resources/export.sql
+++ src/main/resources/export.sql
@@ -1,4 +1,1 @@
-call csvwrite( 'C:\data\export\player.csv', 'select * from player' );
-call csvwrite( 'C:\data\export\season.csv', 'select * from season' );
-call csvwrite( 'C:\data\export\game.csv', 'select * from game' );
-call csvwrite( 'C:\data\export\result.csv', 'select * from result' );
+script drop to 'C:\dev\git\quicksplit\src\main\resources\backup.sql';
--- src/main/resources/insert-transactions.sql
+++ src/main/resources/insert-transactions.sql
@@ -9,18 +9,6 @@ from season s
 inner join game g on g.id_season = s.id_season
 inner join result r on r.id_game = g.id_game
 inner join player p on p.id_player = r.id_player
-where s.id_season <= 14
+where s.id_season = ?
 group by s.id_season, p.id_player
 having am_sum != 0;
-
-/*
- * Inserts a depsoit/withdrawal record one day after each season result transaction.
- * Only to be used for initial data insert, then can be deleted.
- */
-insert into transaction ( id_player, id_season, dt_transaction, am_transaction, tx_description )
-select id_player, null, dt_transaction+1, am_transaction * -1, 'Thanks for paying. Better luck next time.' from transaction
-where id_season is not null and am_transaction < 0;
-
-insert into transaction ( id_player, id_season, dt_transaction, am_transaction, tx_description )
-select id_player, null, dt_transaction+1, am_transaction * -1, 'Withdrawal' from transaction
-where id_season is not null and am_transaction > 0;
